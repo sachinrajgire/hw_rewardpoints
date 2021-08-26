d04bb610405599f906a56902c617ec20c4de3d0a
sachinrajgire <ketakiharpale@Ketakis-MacBook-Pro-2.local>
2021-08-26 05:18:03 -0400

    first draft

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
+import faker from 'faker'
+
+let numberOfMonths= 1
+let numberOfDaysInMonths= 3
+
+const createdRecords = []
+// Basic Assumptions
+// there are 90 days in 3 months and we are doing one transcation a day
+// Total transcation Amount -- 0 < Amount < 500
+
+for(let i=0;i< numberOfDaysInMonths * numberOfMonths ;i++) {
+  createdRecords.push({
+    amount:Math.floor((Math.random() * 500) + 1),
+    transcationType:"purchase",
+    transcationDescription:faker.finance.transactionDescription()
+  })
+}
+
+console.log(createdRecords,'createdRecords');
 
 function App() {
+
+function pointsEarned(amount) {
+if(amount > 100){
+  return (amount - 100)*2 + 50
+}
+if(amount <= 100 && amount >=50){
+  return (amount - 50) *1
+}
+return 0  
+}
+
+const totalPoints =createdRecords.reduce((accu,next)=>{
+  const {amount}= next
+  return accu+pointsEarned(amount)
+},0)  
+
+let recs= createdRecords.map(i=>{
+  const {transcationDescription,transcationType,amount}= i
+return (
+  <div>
+{`${transcationDescription.slice(0,30)}${transcationType}  ${amount} ${pointsEarned(amount)}`}
+  </div>
+)
+})
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        
+        {totalPoints}
+        {recs}
+      
     </div>
   );
 }
